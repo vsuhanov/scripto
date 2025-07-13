a6fcd4cb1cf03bbf5d9882edd1fa4d3bf400fe4c
vitaly <vitalijs.suhanovs@gmail.com>
2025-07-13 23:53:45 +0300

    add RELEASE notes

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "scripto/commands"
+import (
+	"fmt"
+	"os"
+
+	"scripto/commands"
+)
+
+// Version information (set during build)
+var version = "dev"
 
 func main() {
+	// Handle version flag
+	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
+		fmt.Printf("scripto version %s\n", version)
+		return
+	}
+
 	commands.Execute()
 }
